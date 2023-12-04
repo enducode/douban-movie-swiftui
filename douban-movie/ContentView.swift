137ab82b998148d7c05ba5f90dddde093a8ae8a9
wanghaoyu <haoyu.wang@palmpay-inc.com>
2023-12-04 22:08:35 +0800

    Initial Commit

---
+++ douban-movie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  douban-movie
+//
+//  Created by 王浩宇 on 2023/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ douban-movie/douban_movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  douban_movieApp.swift
+//  douban-movie
+//
+//  Created by 王浩宇 on 2023/12/4.
+//
+
+import SwiftUI
+
+@main
+struct douban_movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
